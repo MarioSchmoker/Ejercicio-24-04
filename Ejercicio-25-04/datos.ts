c408bf269296739accba3825171573345b4a1440
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-04-25 23:24:50 -0300

    Initial commit

---
+++ Ejercicio-25-04/datos.ts
@@ -0,0 +1,9 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+rotulo.innerHTML = "Ingrese un dato:";
+btnEnv.addEventListener("click", () => {
+  // TU CODIGO CA AQUI, POR EJEMPLO:
+  console.log("el dato ingresado es: ", dato.value);
+  //
+});
