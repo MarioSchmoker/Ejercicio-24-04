1a5e6478a50a2898d2066bc85949851a52214d31
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-05-01 11:55:29 -0300

    Área del rectángulo

--- Ejercicio-25-04/datos.ts
+++ Ejercicio-25-04/datos.ts
@@ -1,9 +1,27 @@
-let btnEnv = document.getElementById("btnEnviar");
-let dato = document.getElementById("dato");
-let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "Ingrese un dato:";
-btnEnv.addEventListener("click", () => {
+
+//let btnEnv = document.getElementById("btnEnviar");
+let Base = document.getElementById("Base");
+//let rotulo1 = document.getElementById("Base");
+let btnCacular = document.getElementById("btnCalcular");
+let Altura = document.getElementById("Altura");
+//let rotulo2 = document.getElementById("Altura");
+
+rotulo1.innerHTML = "Base";
+//btnCalculo.addEventListener("click", () => {
   // TU CODIGO CA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es: ", dato.value);
-  //
+
+rotulo2.innerHTML = "Altura";
+//btnEnviar.addEventListener("click", () => {
+
+  btnCalcular.addEventListener("click", () => {
+
+    let Base1: number = Number(Base.value);
+    let Altura1 : number = Number(Altura.value);
+    let area : number = Base1 * Altura1;
+     
+    console.log("La Base es: ",Base1;
+    console.log("La Altura es: ",Altura1;
+
+console.log("EL área es:",area); 
+
 });
